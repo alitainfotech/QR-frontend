9d4d432bcf0dcd062360cc81e1843043b97ebf80
Vijay Reshamwala <rvijay@alita.local>
2023-10-07 13:01:23 +0530

    style:phase-2-1 scree-1 done

--- src/components/phase-2-1/screen-1/Index.vue
+++ src/components/phase-2-1/screen-1/Index.vue
@@ -15,11 +15,122 @@
     <div class="contentHeight">
       <div class="px-2">
         <v-card
-          variant="tonal"
-          class="bg-grayLight rounded-lg d-flex flex-column justify-center"
+          variant="flat"
+          color="grayLight"
+          class="rounded-lg d-flex flex-column justify-center"
         >
-         <v-container class="bg-white">fdf
-          </v-container>
+          <div class="pa-3">
+            <DescriptionCard class="mb-4">
+              <template v-slot:row1Col1>
+                <span
+                  class="text-capitalize mb-1 text-secondary text-14-medium"
+                >
+                  Sub-Product: Fabric Code
+                </span>
+                <p class="text-capitalize text-primary text-16-bold">Length</p>
+              </template>
+              <template v-slot:row1Col2>
+                <span
+                  class="text-capitalize mb-1 text-secondary text-14-medium"
+                >
+                  Result :</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">
+                  79 (-1.5)
+                </p>
+              </template>
+              <template v-slot:row2col1>
+                <span class="text-capitalize mb-1 text-secondary text-14-medium"
+                  >Body</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">77.5</p>
+              </template>
+              <template v-slot:row2col2>
+                <span class="text-capitalize mb-1 text-secondary text-14-medium"
+                  >Pattern</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">78.0</p>
+              </template>
+              <template v-slot:row2col3>
+                <span class="text-capitalize mb-1 text-secondary text-14-medium"
+                  >Finished</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">78.0</p>
+              </template>
+              <template v-slot:row3col1>
+                <p class="text-capitalize text-primary text-16-bold">
+                  <span
+                    class="text-capitalize mb-1 text-secondary text-14-medium"
+                    >ALT 1 : </span
+                  >78.0
+                </p>
+              </template>
+              <template v-slot:row3col2>
+                <p class="text-capitalize text-primary text-16-bold">
+                  <span
+                    class="text-capitalize mb-1 text-secondary text-14-medium"
+                    >ALT 1 :
+                  </span>
+                  78.0
+                </p>
+              </template>
+            </DescriptionCard>
+            <DescriptionCard>
+              <template v-slot:row1Col1>
+                <span
+                  class="text-capitalize mb-1 text-secondary text-14-medium"
+                >
+                  Sub-Product: Fabric Code
+                </span>
+                <p class="text-capitalize text-primary text-16-bold">Length</p>
+              </template>
+              <template v-slot:row1Col2>
+                <span
+                  class="text-capitalize mb-1 text-secondary text-14-medium"
+                >
+                  Result :</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">
+                  79 (-1.5)
+                </p>
+              </template>
+              <template v-slot:row2col1>
+                <span class="text-capitalize mb-1 text-secondary text-14-medium"
+                  >Body</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">77.5</p>
+              </template>
+              <template v-slot:row2col2>
+                <span class="text-capitalize mb-1 text-secondary text-14-medium"
+                  >Pattern</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">78.0</p>
+              </template>
+              <template v-slot:row2col3>
+                <span class="text-capitalize mb-1 text-secondary text-14-medium"
+                  >Finished</span
+                >
+                <p class="text-capitalize text-primary text-16-bold">78.0</p>
+              </template>
+              <template v-slot:row3col1>
+                <p class="text-capitalize text-primary text-16-bold">
+                  <span
+                    class="text-capitalize mb-1 text-secondary text-14-medium"
+                    >ALT 1 : </span
+                  >78.0
+                </p>
+              </template>
+              <template v-slot:row3col2>
+                <p class="text-capitalize text-primary text-16-bold">
+                  <span
+                    class="text-capitalize mb-1 text-secondary text-14-medium"
+                    >ALT 1 :
+                  </span>
+                  78.0
+                </p>
+              </template>
+            </DescriptionCard>
+          </div>
         </v-card>
       </div>
     </div>
@@ -39,13 +150,5 @@
   </div>
 </template>
 <script setup>
+import DescriptionCard from "../common/DescriptionCard.vue";
 </script>
-<style scoped>
-.v-table > .v-table__wrapper > table > tbody > tr > td {
-  padding: 10px 15px 10px 15px !important;
-}
-.hover-camera:hover {
-  background-color: #050336;
-  color: white;
-}
-</style>
