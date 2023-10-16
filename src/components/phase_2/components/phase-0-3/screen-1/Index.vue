5735ddb808b2052e65ab610adf36e1bc2e64242f
Vijay Reshamwala <rvijay@alita.local>
2023-10-16 17:10:55 +0530

    style:phase0-3 screen-1 done

---
+++ src/components/phase_2/components/phase-0-3/screen-1/Index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <BaseTitle>
+      <template v-slot:text> ARRIVAL TO HQ| 0.3 </template>
+    </BaseTitle>
+    <BaseSubtitle class="mb-5">
+      <template v-slot:text>
+       THIS GARMENT IS NOW AT HQ
+      </template>
+    </BaseSubtitle>
+
+    <div class="px-5 mb-10">
+      <BaseNumberButton text="01372" />
+    </div>
+
+    <div class="contentHeight">
+      <div class="px-5">
+      <CommonDescriptionTable :items="data"/>
+      </div>
+    </div>
+    <div class="d-flex justify-center px-5 mb-3">
+      <BaseSubmitButton
+        block
+        size="x-large"
+        color="primary"
+        text="MEASUREMENT CHECK | 3.1"
+        icon="mdi-greater-than"
+        variant="elevated"
+        fontSize="text-16-bold"
+        fontColor="text-white"
+        iconSize="small"
+        class="px-2"
+      />
+    </div>
+    <div class="d-flex justify-center px-5 mb-3">
+      <BaseSubmitButton
+        block
+        size="x-large"
+        color="primary-lighten-3"
+        text="QC CHECK | 2.1"
+        icon="mdi-greater-than"
+        variant="elevated"
+        fontSize="text-16-bold"
+        fontColor="text-white"
+        iconSize="small"
+        class="px-2"
+      />
+    </div>
+    <div class="d-flex justify-center px-5" style="opacity:0.5">
+      <BaseSubmitButton
+        block
+        size="x-large"
+        color="primary-lighten-3"
+        text="STYLE DESIGNS CHECK | 1.1"
+        icon="mdi-greater-than"
+        variant="elevated"
+        fontSize="text-16-bold"
+        fontColor="text-white"
+        iconSize="small"
+        class="px-2"
+      />
+    </div>
+  </div>
+</template>
+<script setup>
+import {ref} from 'vue'
+const data = ref([
+  {
+    title: "Order #",
+    value: "01372",
+  },
+  {
+    title: "Factory",
+    value: "B",
+  },
+  {
+    title: "Start Date",
+    value: "07-09-23",
+  },
+  {
+    title: "Due Date",
+    value: "21-9-23",
+  },
+  {
+    title: "Arrival Date",
+    value: "01372",
+  },
+]);
+</script>
--- src/components/phase_2/router/index.js
+++ src/components/phase_2/router/index.js
@@ -1,6 +1,7 @@
 import Phase0_1Screen1 from '../views/phase0-1/Screen1.vue'
 import Phase0_1Screen2 from '../views/phase0-1/Screen2.vue'
 import Phase0_2Screen1 from '../views/phase0-2/Screen1.vue'
+import Phase0_3Screen1 from '../views/phase0-3/Screen1.vue'
 const route =[
     {
         path: '/phase-0-1-screen1',
@@ -16,6 +17,11 @@ const route =[
         path: '/phase-0-2-screen1',
         name: 'phase-0-2-screen1',
         component: Phase0_2Screen1
+      },
+      {
+        path: '/phase-0-3-screen1',
+        name: 'phase-0-3-screen1',
+        component: Phase0_3Screen1
       },
 ]
 export default route
